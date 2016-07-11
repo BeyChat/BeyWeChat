40cb411df6f8a28a482e40c0edf78a8dae6a609b
aidaiyan520 <1046991500@qq.com>
2016-07-11 17:31:23 +0800

    MOD:project

---
+++ BeyWeChat/BeyWeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeyWeChat
+//
+//  Created by Jason Huang on 16/7/11.
+//  Copyright © 2016年 JasonHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeyWeChat/BeyWeChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeyWeChat
+//
+//  Created by Jason Huang on 16/7/11.
+//  Copyright © 2016年 JasonHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
